41c49c5dd1db2a22c429a2648d48f4da92417b32
Thiago Correa <thiago.correa_ntconsult@prestador.globo>
2024-01-05 14:49:37 -0300

    Created campaign and unit tests

---
+++ internal/domain/campaign/campaign.go
@@ -0,0 +1,35 @@
+package campaign
+
+import (
+	"time"
+
+	"github.com/rs/xid"
+)
+
+type Contact struct {
+	Email string
+}
+
+type Campaign struct {
+	ID        string
+	Name      string
+	CreatedAt time.Time
+	Content   string
+	Contacts  []Contact
+}
+
+func BuildCampaign(name string, content string, emails []string) *Campaign {
+
+	contacts := make([]Contact, len(emails))
+	for index, email := range emails {
+		contacts[index].Email = email
+	}
+
+	return &Campaign{
+		ID:        xid.New().String(),
+		Name:      name,
+		CreatedAt: time.Now(),
+		Content:   content,
+		Contacts:  contacts,
+	}
+}
---
+++ internal/domain/campaign/campaign_test.go
@@ -0,0 +1,22 @@
+package campaign
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuildCampaign(t *testing.T) {
+	assert := assert.New(t)
+	name := "Golang"
+	content := "Content"
+	contacts := []string{"email@test", "email2@test"}
+
+	campaign := BuildCampaign(name, content, contacts)
+
+	assert.NotNil(campaign.ID)
+	assert.Equal(campaign.Name, name)
+	assert.NotNil(campaign.CreatedAt)
+	assert.Equal(campaign.Content, content)
+	assert.Equal(len(campaign.Contacts), len(contacts))
+}
